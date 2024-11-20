1df014411ff577e0f4235009191e39253042c270
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-20 10:07:49 +0100

    add not found route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import MainNavBar from './components/MainNavBar';
 
 function App() {
   return (
     <div className='App'>
       <MainNavBar />
+      <Routes>
+        <Route path='*' element={<h1>Not Found</h1>} />
+      </Routes>
     </div>
   );
 }
