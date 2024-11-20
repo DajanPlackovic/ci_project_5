9a821ebe04e6527926616a4436d08edf17b6a2a8
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-20 19:02:43 +0100

    attempt at getting the profile detail view to work

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Profile
+from project_5.permissions import IsOwnerOrReadOnly
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.order_by('-created_at')
+    serializer_class = ProfileSerializer
