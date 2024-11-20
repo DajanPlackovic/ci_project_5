ff38d216a745b65b014921837a8b37cbe8bf8f5a
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-20 19:20:51 +0100

    semi functional profile image todo: fix height

--- frontend/src/components/MainNavBar.jsx
+++ frontend/src/components/MainNavBar.jsx
@@ -1,9 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
 import { useTheme, useSetTheme } from '../contexts/ThemeContext';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { axiosReq } from '../api/axiosDefaults';
 
 function MainNavBar() {
   const theme = useTheme();
@@ -20,6 +23,25 @@ function MainNavBar() {
     console.log(currentUser);
   };
 
+  const [userImage, setUserImage] = useState(null);
+
+  useEffect(() => {
+    const getUserImage = async () => {
+      if (currentUser) {
+        try {
+          const profile = await axiosReq.get(
+            `/profiles/${currentUser?.profile_id}/`
+          );
+          setUserImage(profile.data.image);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+
+    getUserImage();
+  }, [currentUser]);
+
   return (
     <Navbar expand='lg' className='bg-body-tertiary'>
       <Container>
@@ -33,7 +55,9 @@ function MainNavBar() {
               </span>
             </button>
             {currentUser ? (
-              <p>{currentUser.username}</p>
+              <Link className='nav-link'>
+                <Image src={userImage} roundedCircle height='40' />
+              </Link>
             ) : (
               <Link to='/signin'>
                 <span className='material-symbols-outlined'>
