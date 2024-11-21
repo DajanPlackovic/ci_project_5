66771885b6d226450da9669dc4ec07818d1434f3
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-21 16:39:50 +0100

    fix axios default base url for prod

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api';
+// axios.defaults.baseURL = 'http://localhost:8000/api'; // dev
+axios.defaults.baseURL = '/api'; // prod
+
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
