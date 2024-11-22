df6bb64c9a031c433b03ddc79d456870ba2865e9
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-22 19:09:36 +0100

    fix context provider order

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,13 +12,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <ThemeProvider>
-        <CurrentUserProvider>
-          <GlobalErrorProvider>
+      <GlobalErrorProvider>
+        <ThemeProvider>
+          <CurrentUserProvider>
             <App />
-          </GlobalErrorProvider>
-        </CurrentUserProvider>
-      </ThemeProvider>
+          </CurrentUserProvider>
+        </ThemeProvider>
+      </GlobalErrorProvider>
     </Router>
   </React.StrictMode>
 );
