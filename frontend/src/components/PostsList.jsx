d9f9b56e34e2910796aae80725f869751b4f1072
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-22 19:09:17 +0100

    add infinite scroll to posts

--- frontend/src/components/PostsList.jsx
+++ frontend/src/components/PostsList.jsx
@@ -3,6 +3,8 @@ import { axiosRes } from '../api/axiosDefaults';
 import Container from 'react-bootstrap/esm/Container';
 import Spinner from 'react-bootstrap/Spinner';
 import Post from './Post';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { fetchMoreData } from '../utils/utils';
 
 const PostsList = () => {
   const [posts, setPosts] = useState([]);
@@ -10,9 +12,8 @@ const PostsList = () => {
 
   useEffect(() => {
     const getPosts = async () => {
-      const { data } = await axiosRes.get('/posts/');
+      const { data: posts } = await axiosRes.get('/posts/');
       setLoading(false);
-      const { results: posts } = data;
       setPosts(posts);
     };
 
@@ -22,8 +23,18 @@ const PostsList = () => {
     <Spinner role='status' className='m-auto' />
   ) : (
     <Container className='p-2 pt-4'>
-      {posts.length ? (
-        posts.map((post) => <Post key={post.id} post={post} />)
+      {posts.results?.length ? (
+        <InfiniteScroll
+          dataLength={posts.results?.length}
+          loader={<Spinner role='status' className='d-block m-auto' />}
+          hasMore={!!posts.next}
+          next={() => {
+            fetchMoreData(posts, setPosts);
+          }}>
+          {posts.results.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </InfiniteScroll>
       ) : (
         <h1>There are no posts</h1>
       )}
