38fbaa984dd7012988e09e6b00a30b18af0d8217
Dajan Plackovic <plackovicdajan@gmail.com>
2024-11-29 20:08:24 +0100

    try removing Promise.all

--- frontend/src/components/PostDetail.jsx
+++ frontend/src/components/PostDetail.jsx
@@ -30,10 +30,12 @@ const PostDetail = () => {
   useEffect(() => {
     const getPosts = async () => {
       try {
-        const [{ data: post }, { data: comments }] = await Promise.all([
-          axiosRes.get(`/posts/${id}`),
-          axiosRes.get(`/comments/?post=${id}&response_to__isnull=True`),
-        ]);
+        // const [{ data: post }, { data: comments }] = await Promise.all([
+        const { data: post } = await axiosRes.get(`/posts/${id}`);
+        const { data: comments } = await axiosRes.get(
+          `/comments/?post=${id}&response_to__isnull=True`
+        );
+        // ]);
         setPost(post);
         setComments(comments);
       } catch (err) {
